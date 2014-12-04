7ed4e6befb99670ed05e4dfa36352ecacb38d38d
Kevin Shin <kevnshin@gmail.com>
2014-12-03 21:14:04 -1000

    Created click listeners for About and Spec Links

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,9 +1,24 @@
 $(function() {
 
-$.get( "/api/about.json", function( data ) {
-  
-  $("div#description p").append(data.body);
+  var about;
+  var spec;
 
-});
+  $.get( "/api/about.json", function( data ) {
+    about = data.body;
+    $("div#description p").append(about);
+  });
 
-});
+  $.get( "/api/spec.json", function( data ) {
+    spec = data.body;
+  });//ends GET request for spec
+
+  $("div#large_navbar ul.links_list li.spec_link a").click(function(){
+    $("div#description p").html("");
+    $("div#description p").append(spec);
+  });//ends onClick listener for Spec
+
+  $("div#large_navbar ul.links_list li.about_link a").click(function(){
+    $("div#description p").html("");
+    $("div#description p").append(about);
+  });//ends onClick listener for About
+});//ends entire function
