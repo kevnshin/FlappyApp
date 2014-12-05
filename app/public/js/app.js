a37f811be95beb6fcd41d00247e0575c50602ec0
Kevin Shin <kevnshin@gmail.com>
2014-12-04 17:38:48 -1000

    Finished adding items to footer using jQuer

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,34 +1,111 @@
 $(function() {
 
-  var about;
-  var spec;
+  var footerLinkTitles = [];
 
-  $.get( "/api/about.json", function( data ) {
-    about = data.body;
-    $("div#description p").append(about);
+  //When Page Initiall LOADS, do the following
+  //get about description and load it onto page
+  get_description("/api/about.json");
+
+  //GET request for footer data
+  $.get( "http://beta.json-generator.com/api/json/get/AydVk97", function (data) {
+    
+    //Put footer link titles in an array
+    data.forEach(function (element) {
+      footerLinkTitles.push(element.title);
+    });
+
+    column_length = get_footercolumnLength(footerLinkTitles);
+
+    console.log(column_length);
+
+    for (var i = 0, j = i+column_length, k = j+column_length; i < column_length; i++, j++, k++) {
+      
+      add_column(i, 1);
+      add_column(j, 2);
+      if(footerLinkTitles[k]){
+        add_column(k ,3);
+      }
+    }
   });
 
-  $.get( "/api/spec.json", function( data ) {
-    spec = data.body;
-  });//ends GET request for spec
-
+  //spec link click function
   $("a.spec_link").click(function(){
-    $("div#description p").html("");
-    $("div#description p").append(spec);
-  });//ends onClick listener for Spec
+    get_description("/api/spec.json");
+  });
 
+  //about link click function
   $("a.about_link").click(function(){
-    $("div#description p").html("");
-    $("div#description p").append(about);
-  });//ends onClick listener for About
+    get_description("/api/about.json");
+  });
 
+  //Hover on bird(entering)
   $("div#description").on("mouseenter", "span.bird", function () {
     $(this).html("<div class='smallbird_pic'></div>");
   });
 
+  //Hover on bird(leaving)
   $("div#description").on("mouseleave", "span.bird", function () {
     $(this).html("bird");
   });
 
+  //Funcions
+  //GET request for description area
+  function get_description (url) {
+    $.get( url, function( data ) {
+      $("div#description p").html(data.body);
+    });
+  }
+
+  function get_footercolumnLength (array) {
+
+    switch (array.length%3) {
+
+      case 0:
+        return array.length/3;
+
+      case 1:
+        return (array.length+2)/3;
+
+      case 2:
+        return (array.length+1)/3;
+
+      default:
+        return array.length/3;
+    }
+  }
+
+  function add_column (listNum, columnNum) {
+
+    var link = $("<a href='#'></a>");
+    link.html(footerLinkTitles[listNum]);
+    
+    var list_item = $("<li>");
+    list_item.html(link);
+
+    //first column
+    if(columnNum === 1){
+      var totalNum = column_length;
+      var target = "ul#footer_firstcolumn";
+
+      if (listNum === totalNum - 1){
+        list_item.addClass("last_link");
+      }
+    } else if (columnNum === 2) {// second column
+      var totalNum = column_length * 2;
+      var target = "ul#footer_secondcolumn";
+
+      if (listNum === totalNum - 1){
+        list_item.addClass("last_link");
+      }
+    } else if (columnNum === 3) {//third column
+      var target = "ul#footer_thirdcolumn";  
+      
+      if (!footerLinkTitles[listNum+1]){
+        list_item.addClass("last_link");
+      }
+    }//ends else if
+
+    $("div#footer " + target).append(list_item);
+  }//ends add_column
 
 });//ends entire function
